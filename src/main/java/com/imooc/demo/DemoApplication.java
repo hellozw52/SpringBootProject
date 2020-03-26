0f0fe306315f16aba5579acb55d9e94ed1ff5d93
helloZw <466747863@qq.com>
2020-03-26 15:31:06 +0800

    优化框架配置

---
+++ src/main/java/com/imooc/demo/DemoApplication.java
@@ -0,0 +1,32 @@
+package com.imooc.demo;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.core.env.Environment;
+
+@SpringBootApplication
+@ComponentScan("com")
+public class DemoApplication extends SpringBootServletInitializer {
+
+    private static final Logger LOG = LoggerFactory.getLogger(DemoApplication.class);
+
+    public static void main(String[] args) {
+        SpringApplication app = new SpringApplication(DemoApplication.class);
+        Environment env = app.run(args).getEnvironment();
+        System.out.println("启动成功！！");
+        System.out.println("用户查询: \thttp://127.0.0.1:" + env.getProperty("server.port") + env.getProperty("server.servlet.context-path") + "/user/list");
+        System.out.println("交易查询: \thttp://127.0.0.1:" + env.getProperty("server.port") + env.getProperty("server.servlet.context-path") + "/aliTradingRecord/normalQuery?userId=2088332759128563&commodityName=云南");
+        System.out.println("页面跳转: \thttp://127.0.0.1:" + env.getProperty("server.port") + env.getProperty("server.servlet.context-path") + "/th/index");
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder){
+        return builder.sources(DemoApplication.class);
+    }
+
+}
